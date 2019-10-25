e7cd857209b01c0a730984bb446261e673c77466
Alex Church <alex@church.id.au>
2019-10-25 21:23:04 +1100

    Added lint task.

---
+++ jogger/tasks/lint.py
@@ -0,0 +1,193 @@
+import os
+import sys
+from collections import OrderedDict
+
+from jogger.utils.files import walk
+
+from .base import Task
+
+try:
+    import flake8  # noqa
+    HAS_FLAKE8 = True
+except ImportError:
+    HAS_FLAKE8 = False
+
+try:
+    import isort  # noqa
+    HAS_ISORT = True
+except ImportError:
+    HAS_ISORT = False
+
+try:
+    from django.core.management import call_command  # noqa
+    HAS_DJANGO = True
+except ImportError:
+    HAS_DJANGO = False
+
+ENDINGS = {
+    'CRLF': b'\r\n',
+    'CR': b'\r',
+    'LF': b'\n'
+}
+
+DEFAULT_GOOD_ENDING = 'LF'
+DEFAULT_MAX_FILESIZE = 1024 * 1024  # 1MB in bytes
+
+
+class LintTask(Task):
+    
+    help = (
+        'Lint the project. Automatically detects, and uses if found, isort and '
+        'flake8 for linting Python code. Also runs fable (Find All Bad Line '
+        'Endings) and performs a dry-run of makemigrations (if Django is detected).'
+    )
+    
+    steps = [
+        ('python', '-p', 'Perform linting of Python code.'),
+        ('fable', '-f', 'Find all bad line endings.'),
+        ('migrations', '-m', 'Perform makemigrations dry-run.')
+    ]
+    
+    def __init__(self, *args, **kwargs):
+        
+        super().__init__(*args, **kwargs)
+        
+        self.outcomes = OrderedDict()
+    
+    def add_arguments(self, parser):
+        
+        for name, short_flag, help_text in self.steps:
+            parser.add_argument(
+                short_flag, f'--{name}',
+                action='store_true',
+                dest=f'do_{name}',
+                help=help_text
+            )
+    
+    def log_step(self, intro):
+        
+        self.stdout.write(self.styler.label(intro))
+    
+    def handle(self, **options):
+        
+        settings = self.settings
+        
+        explicit_steps = []
+        implicit_steps = []
+        for step, _, _ in self.steps:
+            # Consider the step explicit if a command line flag is included for
+            # it. Otherwise, consider the step implicit unless it has been
+            # disabled via the project settings file
+            if options[f'do_{step}']:
+                explicit_steps.append(step)
+            elif settings.getboolean(step, fallback=True):
+                implicit_steps.append(step)
+        
+        if explicit_steps:
+            run = explicit_steps
+        else:
+            run = implicit_steps
+        
+        for step in run:
+            getattr(self, f'handle_{step}')()
+        
+        self.log_step('Summary')
+        for label, result in self.outcomes.items():
+            if result:
+                styled_result = self.styler.success('OK')
+            else:
+                styled_result = self.styler.error('FAIL')
+            
+            self.stdout.write(f'{label}: {styled_result}')
+    
+    def handle_python(self):
+        
+        if HAS_ISORT:
+            self.log_step('Running isort...')
+            result = self.cli('isort -rc --check-only --diff .')
+            self.outcomes['isort'] = result.returncode == 0
+            self.stdout.write('')  # newline
+        
+        if HAS_FLAKE8:
+            self.log_step('Running flake8...')
+            result = self.cli('flake8 .')
+            self.outcomes['flake8'] = result.returncode == 0
+            self.stdout.write('')  # newline
+    
+    def _get_fable_excludes(self):
+        
+        # Start with some sane default exclusions
+        excludes = {'.git', '__pycache__', '*.pyc', '*.pdf', '*.png', '*.jpg', '*.jpeg', '*.gif'}
+        
+        # Add any configured excludes
+        try:
+            extra_excludes = self.settings['fable_exclude']
+        except KeyError:
+            pass
+        else:
+            # Split the multiline string into a list, stripping whitespace and
+            # filtering out empty values
+            extra_excludes = [p.strip() for p in extra_excludes.splitlines()]
+            excludes.update(filter(None, extra_excludes))
+        
+        return excludes
+    
+    def handle_fable(self):
+        
+        self.log_step('Running fable...')
+        
+        excludes = self._get_fable_excludes()
+        
+        # Get the appropriate good ending from settings
+        good_ending = self.settings.get('fable_good_endings', DEFAULT_GOOD_ENDING)
+        if good_ending not in ENDINGS:
+            self.stderr.write(f'Invalid value for fable_good_endings setting ({good_ending}).')
+            sys.exit(1)
+        
+        # Compile inverse dictionary of bad line endings
+        bad_endings = {v: k for k, v in ENDINGS.items() if k != good_ending}
+        
+        # Get the maximum file size to analyse from settings
+        max_filesize = self.settings.get('fable_max_filesize', DEFAULT_MAX_FILESIZE)
+        
+        try:
+            max_filesize = int(max_filesize)
+        except ValueError:
+            self.stderr.write(f'Invalid value for fable_max_filesize setting ({max_filesize}).')
+            sys.exit(1)
+        
+        result = True
+        skipped = 0
+        for filename in walk('./', excludes):
+            if os.path.getsize(filename) > max_filesize:
+                skipped += 1
+                continue
+            
+            with open(filename, 'rb') as f:
+                content = f.read()
+                for ending in bad_endings:
+                    if ending in content:
+                        self.stdout.write(f'Detected {bad_endings[ending]}: {filename}')
+                        result = False
+                        break
+        
+        if skipped:
+            self.stdout.write(f'Skipped {skipped} large files')
+        
+        self.outcomes['fable'] = result
+        self.stdout.write('')  # newline
+    
+    def handle_migrations(self):
+        
+        if HAS_DJANGO:
+            self.log_step('\nChecking for missing migrations...')
+            
+            try:
+                call_command('makemigrations', dry_run=True, check=True, stdout=self.stdout, stderr=self.stderr)
+            except SystemExit:
+                result = False
+            else:
+                result = True
+            
+            self.outcomes['fable'] = result
+            self.stdout.write('')  # newline
--- jogger/utils/files.py
+++ jogger/utils/files.py
@@ -1,4 +1,5 @@
 import os
+from fnmatch import fnmatch as std_fnmatch
 
 
 def find_file(target_file_name, from_path, max_search_depth=16):
@@ -36,3 +37,70 @@ def find_file(target_file_name, from_path, max_search_depth=16):
         raise FileNotFoundError(f'Could not find {target_file_name}.')
     
     return matched_file
+
+
+def fnmatch(filename, patterns):
+    """
+    Test whether the ``filename`` string matches any of the strings in
+    ``patterns``, using the standard library ``fnmatch()`` function internally
+    to test each pattern. Return ``True`` if the filename matches or ``False``
+    if it does not.
+    
+    :param filename: The filename to test.
+    :param patterns: An iterable of patterns to test against.
+    :return: ``True`` if a match is found, ``False`` if not.
+    """
+    
+    return any(std_fnmatch(filename, pattern) for pattern in patterns)
+
+
+def pathmatch(path, patterns):
+    """
+    Test whether the ``path`` string matches any of the strings in ``patterns``.
+    ``path`` can be a relative file path, with both its basename and absolute
+    path tested against ``patterns``.
+    
+    :param path: The file path to test.
+    :param patterns: An iterable of patterns to test against.
+    :return: ``True`` if a match is found, ``False`` if not.
+    """
+    
+    basename = os.path.basename(path)
+    if fnmatch(basename, patterns):
+        return True
+    
+    absolute_path = os.path.abspath(path)
+    return fnmatch(absolute_path, patterns)
+
+
+def walk(from_path, exclude_patterns=None):
+    """
+    Yield all filenames under the ``from_path`` directory, optionally excluding
+    any files/directories matching any of the pattern strings in
+    ``exclude_patterns``.
+    
+    :param from_path: The root directory to walk.
+    :param exclude_patterns: An iterable of patterns to test against.
+    """
+    
+    if not exclude_patterns:
+        # No exclusion patterns, perform simple directory walk
+        for root, dirs, files in os.walk(from_path):
+            for filename in files:
+                yield os.path.join(root, filename)
+    else:
+        # Perform more complex directory walk, excluding files/directories
+        # matching given exclusion patterns
+        for root, dirs, files in os.walk(from_path, topdown=True):
+            # Removing items from `dirs` will prevent `os.walk` from entering
+            # those subdirectories. Iterate a copy so removals don't result in
+            # an early exit from the loop.
+            for directory in dirs.copy():
+                joined = os.path.join(root, directory)
+                if pathmatch(joined, exclude_patterns):
+                    dirs.remove(directory)
+            
+            for filename in files:
+                joined = os.path.join(root, filename)
+                if not pathmatch(joined, exclude_patterns):
+                    yield joined
