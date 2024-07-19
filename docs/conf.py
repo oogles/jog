da54f5ebbb510f0a1acea68e4c2da2c8f11ac610
Alex Church <alex@church.id.au>
2024-07-19 11:40:38 +1000

    Updated Sphinx config to correctly set theme for RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ nitpicky = True
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx_inline_tabs',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,14 +54,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-# Only import and set the RTD theme if we're building docs locally. Otherwise,
-# readthedocs.org uses their theme by default, so no need to specify it.
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
