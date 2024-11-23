dc08e0e028e5f6d92be364b6d1248ebbd11136e9
Alex Church <alex@church.id.au>
2024-11-23 15:13:23 +1100

    Bumped version to 2.0.2.

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2024, Alex Church'  # noqa: A001 (builtin shadowing)
 author = 'Alex Church'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '2.0.2'
 version = '2.0'
 
 
